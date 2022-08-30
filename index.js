21b7b2e39b081fb3ef062769689185214dd97580
EliotPinto <eliotpintor@gmail.com>
2022-08-29 21:31:51 -0500

    boton y mensaje agregados para capturar pokemones

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ const cartasPokemon = (pokemones) => {
         let randomClase = Math.round(Math.random() * 1 + 5); 
         let randomSerie = Math.round(Math.random() * 150); 
         let pokemon = creandoPokemon(`${i}`, `${randomClase}`, `${randomSerie}`,`${pokemones[i]}`);
-        let div = document.createElement("DIV");
-        console.log(div); 
+        let div = document.createElement("DIV"); 
         div.classList.add("cards"); 
         div.innerHTML = pokemon[0] + pokemon[1] + pokemon[2] + pokemon[3] + pokemon[4]; 
         documentFragment.appendChild(div); 
@@ -30,16 +29,6 @@ const pokemones = ["001","002","003","004","005"]
 cartasPokemon(pokemones);
 
 const form = document.getElementById("formm");
-
-
-class pokedex {
-    constructor (nombre, tipo, id) {
-        this.nombre = nombre;
-        this.tipo = tipo;
-        this.id = id;
-    }
-}
-
 
 
 const enviarFormm = (event) => {
@@ -55,13 +44,34 @@ const enviarFormm = (event) => {
     if (name.value.length, imagen.value.length === 0) alert ("Los datos no son validos, verifique") 
         
     pokemones.push (imagen.value)
-    console.log(pokemones)
     myBox.innerHTML=""
     cartasPokemon (pokemones)
 }
-console.log(form)
-
 form.addEventListener("submit", enviarFormm);
+
+
+let localBase = localStorage.setItem ('nombre', 'idPokemon')
+
+let recuperoStorage = localStorage.getItem('nombre')
+
+const catchAlert = document.querySelector('.bttn')
+
+catchAlert.onclick = (e) => {
+    swal({
+    title: "Wild Pokémon appeared!",
+    buttons: ["Cancel", "Throw a pokeball!" ]
+})
+    .then((result) => {
+        if(result){
+            swal({
+                text: "Pokémon just added to the list",
+                icon: "success",
+        })}
+    })
+};
+
+
+
 
 
 
